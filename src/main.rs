4a588ef22691dff43d1c5512e7cfad18f3e40635
MAK <aqib.khan.25@iiee.edu.pk>
2019-12-04 21:07:32 +0500

    final

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+// use std::fmt::Display;
+pub struct Numbers <T>
+{
+    x: T,
+    y: T,
+}
+
+pub trait Addition <T>
+{
+    fn add(self) -> T;
+}
+
+impl <T> Addition <T> for Numbers <T> 
+{
+    fn add(self) -> T
+    {
+       self.x + self.y 
+    }
+}
+
+fn main() 
+{
+    let number_list = Numbers
+    {
+        x: 5,
+        y: 10,
+    };
+
+    println!("{}", number_list.add());
 }
